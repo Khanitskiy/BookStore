f8cdf5fa5bbeb8f15ef949014c07077f9aade7f0
Khanitskiy <khanitskiy@gmail.com>
2016-03-02 19:41:51 +0200

    back config ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,9 +3,7 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    if user
-        
-    elsif user.admin?
+    if user.admin?
       can :access, :rails_admin       # only allow admin users to access Rails Admin
       can :dashboard           
       can :manage, :all
