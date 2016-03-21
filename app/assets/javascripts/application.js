b6740535413ac7e959544610b726f1cbf3d331b9
Khanitskiy <khanitskiy@gmail.com>
2016-03-21 17:22:40 +0200

    Completion of the js addition in the shop cart

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,12 +18,51 @@
 //= require_tree .
 //= require jquery-star-rating
 
+function setCookie(value) {
+	var expireDate = new Date();
+	expireDate.setDate(expireDate.getDate() + 31);
+
+  //expires = "; expires="+date.toGMTString();
+	document.cookie='books= '+ encodeURIComponent(value) +' ; expires=' + expireDate.toGMTString() + '; path=/';
+}
+
+function changeCookie(book_id, quantity) {
+	var cookie_value = document.cookie.replace(/(?:(?:^|.*;\s*)books\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+
+	if (cookie_value) {
+		var obj = jQuery.parseJSON(decodeURIComponent(cookie_value));
+
+		if (obj["id_" + book_id] != undefined) {
+			obj["id_" + book_id] = (+quantity + parseInt(obj["id_" + book_id])).toString();
+		} else {
+			obj["id_" + book_id] = quantity;
+		}
+
+		obj.book_count = (+quantity + parseInt(obj.book_count)).toString();
+
+		setCookie(JSON.stringify(obj));
+
+		return obj.book_count;
+
+	} else {
+		setCookie("{\"book_count\" : \"" + quantity + "\", \"id_" + book_id + "\":\"" + quantity + "\"}");
+		
+		return quantity;
+	}
+
+}
+
+
 
 $(document).on("ready page:load",function(){
 
 	$(".add_book").click(function(){  
 
 			var book_id = $(this).attr('id').replace("button_book_id_","");
+			var quantity = $('.quantity_id_' + book_id).val();
+
+			// Set cookies
+			var count = changeCookie(book_id, quantity);
 
 			var position_obj = $(".cart-lnk").position();
 			var now_element = $(".book_id_" + book_id + " img").offset();
@@ -40,6 +79,14 @@ $(document).on("ready page:load",function(){
                       width: 50,   
                       height: 50}, 700, function() {  
               $(this).remove();  
+
+              var count_products = ($('.cart-lnk em').text()).slice(1, -1);
+
+              if (count >= 100) {
+              	$(".cart-lnk em").text("(99+)") 
+              } else {
+              	$(".cart-lnk em").text("(" + count + ")");
+              } 
         }); 
     return false; 
    });  
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,5 +3,21 @@ module ApplicationHelper
   def cp(path)
   	"class=active" if current_page?(path)
   end
+
+  def count_products
+
+  	@count_products = JSON.parse(cookies[:books])["book_count"] if cookies[:books]
+  	
+  	if @count_products && @count_products != "0" 
+  		
+  		unless @count_products.to_i >= 100
+  			@count_products
+  		else
+  			"99+"
+  		end		
+  	else
+  		"empty"
+  	end
+  end
   
 end
